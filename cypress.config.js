77d91fc71faae5368ff3fa6283d155a9ccea70fe
blackgrouse <alan.canlin@squaredup.com>
2024-10-29 13:44:57 +0000

    Add cypress-terminal-report

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,14 @@ module.exports = defineConfig({
     baseUrl: cypressEnv.BASE_URL,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    require('cypress-terminal-report/src/installLogsPrinter')(on, {
+        includeSuccessfulHookLogs: true, // Log out all before all and after all hooks
+        outputRoot: config.projectRoot, // required if using outputTarget
+        specRoot: 'cypress/e2e', // Spec root relative to package.json
+        outputTarget: {
+            'reports/mochareports/assets/terminalreport|ts.txt': 'txt' // Log specs to separate files in {directory|extension} in text format
+        }
+    })
     },
   },
 });
