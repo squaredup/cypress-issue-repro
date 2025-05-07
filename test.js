986d47825dd294c8b8cb375cadb66d05b2811674
Shaswot Subedi <shaswot.subedi@squaredup.com>
2025-05-07 13:47:07 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const userInput = "2 + 2";
+const result = eval(userInput);
+console.log("The result is:", result);
+
+
+const GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyzABCD";
+
+const AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
